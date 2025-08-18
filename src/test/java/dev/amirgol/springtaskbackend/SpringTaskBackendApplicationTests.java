3cdb435c154c6d563e652e534ac293e67c7b2548
Amir-Golmoradi <LeoQueenzer2.0@gmail.com>
2025-08-18 04:41:51 +0330

    Add unit test for SpringTaskBackend application context loading

---
+++ src/test/java/dev/amirgol/springtaskbackend/SpringTaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.amirgol.springtaskbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTaskBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
