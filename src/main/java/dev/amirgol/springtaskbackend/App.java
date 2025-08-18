549c96cc53ad22fcecaffb4e62a377e78285baa8
Amir-Golmoradi <LeoQueenzer2.0@gmail.com>
2025-08-18 06:30:48 +0330

    Add main Application class to bootstrap Spring Boot application with logging

---
+++ src/main/java/dev/amirgol/springtaskbackend/App.java
@@ -0,0 +1,16 @@
+package dev.amirgol.springtaskbackend;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        log.info("Starting Application ... ");
+        SpringApplication.run(App.class, args);
+    }
+
+}
