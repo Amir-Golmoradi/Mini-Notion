7edb6ba15799eb913f7f7beb52e79fd375084efa
Amir-Golmoradi <LeoQueenzer2.0@gmail.com>
2025-08-18 04:30:26 +0330

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.4"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "dev.amirgol"
+version = "1.0.0"
+description = "spring-task-backend"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Spring Boot Starters
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // Database
+    implementation("org.postgresql:postgresql")
+    implementation("org.flywaydb:flyway-core")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // Environment Variable
+    implementation("me.paulschwarz:spring-dotenv:3.0.0")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic")
+
+
+
+    // Testing
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.mockito:mockito-core")
+    testImplementation("org.mockito.kotlin:mockito-kotlin")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
