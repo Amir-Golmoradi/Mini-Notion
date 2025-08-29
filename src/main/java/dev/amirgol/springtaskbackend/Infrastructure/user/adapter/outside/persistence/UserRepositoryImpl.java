4a1203a0cadf629ae5710525cbe2f24c8ef09f31
Amir-Golmoradi <LeoQueenzer2.0@gmail.com>
2025-08-30 00:58:57 +0330

    Add UserRepositoryImpl class to implement user persistence logic, including methods for retrieving, saving, and checking existence of users by name and email.

---
+++ src/main/java/dev/amirgol/springtaskbackend/Infrastructure/user/adapter/outside/persistence/UserRepositoryImpl.java
@@ -0,0 +1,59 @@
+package dev.amirgol.springtaskbackend.Infrastructure.user.adapter.outside.persistence;
+
+import dev.amirgol.springtaskbackend.Application.user.dto.mapper.UserDtoMapper;
+import dev.amirgol.springtaskbackend.Domain.user.model.User;
+import dev.amirgol.springtaskbackend.Domain.user.repository.UserRepository;
+import dev.amirgol.springtaskbackend.core.exception.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+    private final UserJpaRepository jpaRepository;
+    private final UserDtoMapper mapper;
+
+    @Override
+    public Page<User> getUsersByNameAndEmail(String name, String email, Pageable pageable) {
+        return jpaRepository.findByNameAndEmail(name, email, pageable);
+    }
+
+    @Override
+    public Optional<User> findUserById(Short id) {
+        return Optional.of(
+                jpaRepository
+                        .findById(id)
+                        .orElseThrow(() -> new ResourceNotFoundException("User not found"))
+        );
+    }
+
+    @Override
+    public User saveUser(User user) {
+        jpaRepository.save(user);
+        return user;
+    }
+
+    @Override
+    public boolean existsByName(String name) {
+        return jpaRepository.existsUserByName(name);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return jpaRepository.existsUserByEmail(email);
+    }
+
+    @Override
+    public Optional<User> findUsersByEmail(String email) {
+        return jpaRepository.findUsersByEmail(email);
+    }
+
+    @Override
+    public Optional<User> findUsersByName(String name) {
+        return jpaRepository.findUsersByName(name);
+    }
+}
