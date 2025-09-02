4a25064285a4c4e770d0248be1f9a1ddc1331893
Amir-Golmoradi <LeoQueenzer2.0@gmail.com>
2025-09-03 02:25:49 +0330

    Update Spring Boot version in build.gradle.kts to 3.3.1, comment out security starter dependency, and adjust test dependencies to include Testcontainers for PostgreSQL.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.5.4"
+    id("org.springframework.boot") version "3.3.1"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -24,14 +24,14 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
     // Spring Boot Starters
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+//    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-devtools")
-//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     // Database
     implementation("org.postgresql:postgresql")
@@ -58,16 +58,15 @@ dependencies {
     // Logging
     implementation("ch.qos.logback:logback-classic")
 
-
-
-    // Testing
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.mockito:mockito-core")
-    testImplementation("org.mockito.kotlin:mockito-kotlin")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
-
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    runtimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {
