99e3820e73229b0d2b79c0b9e91e94f3d3a19dd5
Amir-Golmoradi <LeoQueenzer2.0@gmail.com>
2025-09-03 02:29:28 +0330

    Add migration scripts to create and populate test_users table in H2 database

---
+++ src/main/resources/db/migrations/V7__Create_test_users_table_in_h2_database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE test_users
+(
+    id    SMALLINT PRIMARY KEY,
+    name  VARCHAR(50)  NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
---
+++ src/main/resources/db/migrations/V8_Insert_values_into_test_users_table.sql
@@ -0,0 +1,2 @@
+INSERT INTO "test_users" ("id", "name", "email") VALUES (1, 'Lucy Hale', 'lucyhale@example.com');
+INSERT INTO "test_users" ("id", "name", "email") VALUES (2, 'John Snow', 'johnsnow@example.com');
