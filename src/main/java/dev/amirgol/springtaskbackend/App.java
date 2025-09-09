b4fea6cf8eaceac5cf587d040d42496c0e9802e1
Amir-Golmoradi <LeoQueenzer2.0@gmail.com>
2025-09-09 16:07:47 +0330

    Add Spring Modulith and MinIO properties configuration to App class for enhanced modularity and attachment management.

--- src/main/java/dev/amirgol/springtaskbackend/App.java
+++ src/main/java/dev/amirgol/springtaskbackend/App.java
@@ -1,14 +1,20 @@
 package dev.amirgol.springtaskbackend;
 
+import dev.amirgol.springtaskbackend.attachment.domain.model.Attachment;
+import dev.amirgol.springtaskbackend.attachment.infrastructure.config.MinioProperties;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.modulith.Modulith;
 
 @Slf4j
+@Modulith
 @SpringBootApplication
+@EnableConfigurationProperties(MinioProperties.class)
 public class App {
     public static void main(String[] args) {
-
         log.info("Starting Application ... ");
         SpringApplication.run(App.class, args);
     }
