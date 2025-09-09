2c2eaeff79fc0069258e82391398f08a3ef8490e
Amir-Golmoradi <LeoQueenzer2.0@gmail.com>
2025-09-09 16:21:01 +0330

    Add User entity and Role enum: introduce User class for user management with JPA integration and Role enum for user roles, enhancing user functionality in the application.

---
+++ src/main/java/dev/amirgol/springtaskbackend/user/domain/enums/Role.java
@@ -0,0 +1,6 @@
+package dev.amirgol.springtaskbackend.user.domain.enums;
+
+public enum Role {
+    ADMIN,
+    USER,
+}
---
+++ src/main/java/dev/amirgol/springtaskbackend/user/domain/model/User.java
@@ -0,0 +1,59 @@
+package dev.amirgol.springtaskbackend.user.domain.model;
+
+import dev.amirgol.springtaskbackend.task.domain.model.Task;
+import dev.amirgol.springtaskbackend.user.domain.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User entity that implements UserDetails for Spring Security integration
+ * This entity represents a user in our system and provides authentication details
+ */
+@Builder
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(name = "unique_email", columnNames = "email")})
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Short id;  // SMALL SERIAL
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String passwordHash;
+
+    @Builder.Default
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime createdAt = OffsetDateTime.now();
+
+    @Builder.Default
+    @Column(nullable = false)
+    private OffsetDateTime updatedAt = OffsetDateTime.now();
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks = new ArrayList<>();
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = OffsetDateTime.now();
+    }
+
+}
