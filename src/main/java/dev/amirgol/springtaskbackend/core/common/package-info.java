e65b32a6a6355900216151d1282eacfd02dffd47
Amir-Golmoradi <LeoQueenzer2.0@gmail.com>
2025-09-12 21:39:18 +0330

    Add package-info files for Common and Constants modules: introduce package metadata with NamedInterface annotations to improve module organization and clarity in the project structure.

---
+++ src/main/java/dev/amirgol/springtaskbackend/core/common/package-info.java
@@ -0,0 +1,4 @@
+@NamedInterface("Common")
+package dev.amirgol.springtaskbackend.core.common;
+
+import org.springframework.modulith.NamedInterface;
---
+++ src/main/java/dev/amirgol/springtaskbackend/core/constants/package-info.java
@@ -0,0 +1,4 @@
+@NamedInterface("Constants")
+package dev.amirgol.springtaskbackend.core.constants;
+
+import org.springframework.modulith.NamedInterface;
